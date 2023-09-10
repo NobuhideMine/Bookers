f05d79b9d44c65d56e2f3b21cddbd44042333b28
NobuhideMine <141533543+NobuhideMine@users.noreply.github.com>
2023-09-10 05:57:11 +0000

    [Update] 3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -15,7 +16,7 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
+    @books = Book.all.order(id: "DESC")
   end
 
   def show
@@ -29,7 +30,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render :edit
